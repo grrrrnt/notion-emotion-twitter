b3112180af3a15da4a9936881975212a48661bfd
Grant <37563633+grrrrnt@users.noreply.github.com>
2021-03-15 22:07:12 +0800

    Uploaded dataset & codebase

---
+++ notion-emotion-twitter.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import nltk
+import re
+
+def preprocess(tweets):
+    print("preprocess")
+    
+def main():
+    # NOTE: Need to split train and test set
+    train = pd.read_csv('text_emotion.csv')
+    print("main")
+
+if __name__ == "__main__":
+    main()
