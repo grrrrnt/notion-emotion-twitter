ca4c35a08b224f4c79c44ffb261a1a5a1ac3f207
SimNee <chua_sim_nee@hotmail.com>
2021-03-20 16:54:22 +0800

    added first 2 functions

--- notion-emotion-twitter.py
+++ notion-emotion-twitter.py
@@ -4,10 +4,24 @@ import re
 
 def preprocess(tweets):
     print("preprocess")
-    
+
+def remove_mention(tweet):
+    # Removing tweets with mentions of other users
+    # The username will be removed with the @ and be replaced with an empty string
+    return re.sub(r'@[a-zA-Z0-9_]+', '', tweet)
+
+def alphanumerify(tweet):
+    # Removing any characters that are not alphabet or number
+    # Only alphabets and number will remains, the rest will be replaced with a space
+    return re.sub(r'[^a-zA-Z0-9]+', ' ', tweet)
+
 def main():
     # NOTE: Need to split train and test set
     train = pd.read_csv('text_emotion.csv')
+    # "tweet_id","sentiment","author","content"
+    test_string = "@tiffanylue 123 word he's what! ice-cream @tai_ping"
+    print(alphanumerify(remove_mention(test_string)))
+
     print("main")
 
 if __name__ == "__main__":
