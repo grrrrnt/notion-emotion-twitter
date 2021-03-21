04805d5ff925c5d31b348e162e8d746687d72121
Junwei <junwei.97@hotmail.com>
2021-03-21 21:37:16 +0800

    Add language detection method

--- notion-emotion-twitter.py
+++ notion-emotion-twitter.py
@@ -2,6 +2,17 @@ import pandas as pd
 import nltk
 import re
 
+from langdetect import detect
+
+## Method to check if a given text is in English
+## NOTE: Very short-text can give unexpected results, emoticons should be removed before calling this method
+def isEnglish(text):
+    language = detect(text)
+    if (language == en):
+        return True
+    else:
+        return False
+
 def preprocess(tweets):
     print("preprocess")
     
